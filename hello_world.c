ca21d9ea70cd3e39370bbd071aa02c662d72adeb
PirngruberChris <christianpirngruber3@gmail.com>
2018-09-20 11:33:47 +0200

    Block Header added

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,17 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2dhif
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			Hello World
+ * Author:			Christian Pirngruber
+ * ----------------------------------------------------------
+ * Description:
+ * Prints Hello World
+ * ----------------------------------------------------------
+ */
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  printf("Hello, World\n");
+  printf("Hello World\n");
   return 0;
 }
